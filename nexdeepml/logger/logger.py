c020e3097d78f7c33a8f56ebb57d56eb0b8ac762
ariaameri <ariaameri@live.com>
2020-07-21 15:45:24 -0400

    Adds TQDM logging.

--- nexdeepml/logger/logger.py
+++ nexdeepml/logger/logger.py
@@ -1,4 +1,9 @@
+from typing import Dict
 import logging
+import sys
+from tqdm import tqdm
+import numpy as np
+import io
 
 
 class Logger:
@@ -102,3 +107,115 @@ class Logger:
 
         self._level = level
         self._handler.level = level
+
+
+class TQDMLogger(Logger, io.StringIO):
+
+    # TODO: Write the doc for the config argument
+
+    def __init__(self, config):
+
+        # Making sure the logger is going to write to the console
+        config.console = True
+
+        Logger.__init__(self, config)
+        io.StringIO.__init__(self)
+
+        self._tqdm = tqdm(
+            position=self._counter[0],
+            bar_format='{percentage:3.0f}%|{bar}{r_bar} {desc}',
+            file=self  # Write to this log handler instead of stderr
+        )
+        # Bookkeeping for tqdm
+        self.buf: str = ''
+
+        # The number of total iterations
+        self._total: int = -1
+
+        # The number of total epochs
+        self._n_epochs: int = config.n_epochs
+
+    def set_total(self, total: int) -> None:
+        """Set the total number of iterations and resets the tqdm.
+
+        Also, the number is updated in self._config.total
+
+        Parameters
+        ----------
+        total : int
+            the total number of iterations expected."""
+
+        self._tqdm.reset(total=total)
+        self._total = total
+        self._config.total = total
+
+    def close(self) -> None:
+        """Finishes and closes the tqdm instance."""
+
+        self._tqdm.close()
+
+    def update(self, update_count: int, msg=None) -> None:
+        """Update the tqdm progress bar with description set to message.
+
+        Parameters
+        ----------
+        update_count : int
+            The amount that should be added to the tqdm instance.
+        msg : Any, optional
+            Contains the message to be set as the progress bar description.
+            Will be passed to the _generate_message method.
+        """
+
+        self._tqdm.update(update_count)
+
+        message = self._generate_message(msg)
+        self._tqdm.set_description(message)
+
+    def _generate_message(self, msg: Dict) -> str:
+        """Generates a string based on the input to be used as tqdm bar description.
+
+        If msg is None, empty string will be returned.
+
+        Parameters
+        ---------
+        msg : Dict
+            Dictionary containing the information to be used. Contains:
+                epoch: int
+                loss: float
+                val_loss: float, optional
+        """
+
+        message = ''
+
+        if msg is None:
+            return message
+
+        # Find the length of the total epochs
+        # and reformat the string accordingly
+        ep_len = int(np.ceil(np.log10(self._n_epochs)))
+        message += f'Epoch {msg["epoch"]:{ep_len}d}/{self._n_epochs}: '
+
+        message += f'loss: {msg["loss"]: .5e}'
+
+        if 'val_loss' in msg.keys():
+            message += f', val_loss: {msg["val_loss"]: .5e}'
+
+        # Print the rest in msg
+        for key in [key for key in msg.keys() if key != 'epoch' and key != 'loss' and key != 'val_loss']:
+            message += f', {key}: {msg[key]}'
+
+        message += ' '
+
+        return message
+
+    def write(self, buf: str) -> None:
+        """For tqdm to write to the buffer."""
+
+        self.buf = buf.strip('\r\n\t ')
+
+    def flush(self) -> None:
+        """For tqdm.
+
+        Will write tqdm messages as infos."""
+
+        self._logger.info(self.buf)
