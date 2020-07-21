079bb52c1b6b3bfcd75da0ce125bbb9009de6432
ariaameri <ariaameri@live.com>
2020-07-21 16:27:17 -0400

    Initial tqdm callback commit.

--- nexdeepml/callback/callback.py
+++ nexdeepml/callback/callback.py
@@ -177,3 +177,42 @@ class Callback:
         """
 
         raise NotImplemented
+
+
+class TQDMCallback(Callback):
+    """Creates and manages an instance of tqdm to take care of progress bar for training/testing the network."""
+
+    def __int__(self, tqdm_config):
+        """Initialize the callback for tqdm progress bar."""
+        super().__init__()
+
+        self._tqdm = TQDMLogger(tqdm_config)
+        
+    def on_epoch_begin(self, info: Dict):
+        """Sets the total number of iterations and resets the tqdm progress bar.
+
+        Parameters
+        ----------
+        info : Dict
+            Dictionary containing the info:
+                total_iterations : int
+                    Total number of iterations in each epoch.
+        """
+
+        total_iterations: int = info['total_iterations']
+        self._tqdm.set_total(total_iterations)
+
+    def on_batch_end(self, info: Dict):
+        """Updates the tqdm progress bar.
+
+        Parameters
+        ----------
+        info : Dict
+            Dictionary containing the info:
+                batch_size : int
+                    Batch size to update the progress.
+                For other entries in info, see TQDMLogger.
+        """
+
+        batch_size: int = info.pop('batch_size')
+        self._tqdm.update(batch_size, info)
