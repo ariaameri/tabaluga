033e73305c1d531e573833372ca0397dc0b0b3eb
ariaameri <ariaameri@live.com>
2020-07-27 11:25:52 -0400

    Adds TQDM progress bar for validation as well in TQDMCallback.

--- nexdeepml/callback/callback.py
+++ nexdeepml/callback/callback.py
@@ -1,6 +1,7 @@
+from ..base import base
+from ..util.config import ConfigParser
 from typing import Dict
 from ..logger.logger import TQDMLogger
-from ..base import base
 
 
 class Callback(base.BaseWorker):
@@ -10,8 +11,6 @@ class Callback(base.BaseWorker):
         """Initializes the callback class."""
 
         super().__init__()
-
-# TODO: Do we get to save a config?
 
 
 class CallbackManager(base.BaseManager):
@@ -26,14 +25,39 @@ class CallbackManager(base.BaseManager):
 class TQDMCallback(Callback):
     """Creates and manages an instance of tqdm to take care of progress bar for training/testing the network."""
 
-    def __init__(self, tqdm_config):
+    def __init__(self, tqdm_config: ConfigParser):
         """Initialize the callback for tqdm progress bar."""
         super().__init__()
 
-        self._tqdm = TQDMLogger(tqdm_config)
+        # Save tqdm config
+        self._tqdm_config: ConfigParser = tqdm_config
+
+        # Set the tqdm instances
+        self._train_tqdm: TQDMLogger
+        self._val_tqdm: TQDMLogger
+
+    def on_train_begin(self, info: Dict):
+        """Creates the tqdm train instance."""
+
+        self._train_tqdm: TQDMLogger = TQDMLogger(self._tqdm_config)
+
+    def on_train_end(self, info: Dict):
+        """Closes the tqdm train instance."""
+
+        self._train_tqdm.close()
+
+    def on_val_begin(self, info: Dict):
+        """Creates the tqdm validation instance."""
+
+        self._val_tqdm: TQDMLogger = TQDMLogger(self._tqdm_config)
+
+    def on_val_end(self, info: Dict):
+        """Closes the tqdm validation instance."""
+
+        self._val_tqdm.close()
 
     def on_epoch_begin(self, info: Dict):
-        """Sets the total number of iterations and resets the tqdm progress bar.
+        """Sets the total number of iterations and resets the tqdm train progress bar.
 
         Parameters
         ----------
@@ -44,10 +68,10 @@ class TQDMCallback(Callback):
         """
 
         total_iterations: int = info['total_iterations']
-        self._tqdm.reset(total_iterations)
+        self._train_tqdm.reset(total_iterations)
 
     def on_batch_end(self, info: Dict):
-        """Updates the tqdm progress bar.
+        """Updates the tqdm train progress bar.
 
         Parameters
         ----------
@@ -59,4 +83,33 @@ class TQDMCallback(Callback):
         """
 
         batch_size: int = info.pop('batch_size')
-        self._tqdm.update(batch_size, info)
+        self._train_tqdm.update(batch_size, info)
+
+    def on_val_epoch_begin(self, info: Dict):
+        """Sets the total number of iterations and resets the tqdm validation progress bar.
+
+        Parameters
+        ----------
+        info : Dict
+            Dictionary containing the info:
+                total_iterations : int
+                    Total number of iterations in each epoch.
+        """
+
+        total_iterations: int = info['total_iterations']
+        self._val_tqdm.reset(total_iterations)
+
+    def on_val_batch_end(self, info: Dict):
+        """Updates the tqdm validation progress bar.
+
+        Parameters
+        ----------
+        info : Dict
+            Dictionary containing the info:
+                batch_size : int
+                    Batch size to update the progress.
+                For other entries in info, see TQDMLogger.
+        """
+
+        batch_size: int = info.pop('batch_size')
+        self._val_tqdm.update(batch_size, info)
