d6d3c37f7a275cf1b9be85ffd962d6fc24be6904
ariaameri <ariaameri@live.com>
2020-07-27 10:28:28 -0400

    Adds base model classes.

---
+++ nexdeepml/model/model.py
@@ -0,0 +1,23 @@
+from ..util.config import ConfigParser
+from ..base import base
+
+
+class ModelManager(base.BaseManager):
+    """Abstract class that manages neural network models."""
+
+    def __init__(self):
+        """Initializes this instance."""
+
+        super().__init__()
+
+
+class Model(base.BaseWorker):
+    """Abstract class that contains the neural network model."""
+
+    def __init__(self):
+        """Initializes the instance.
+
+        Creates the model and takes care of initial tasks.
+        """
+
+        super().__init__()
