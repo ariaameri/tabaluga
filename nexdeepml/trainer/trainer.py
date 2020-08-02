52ea5fb979b41727ee530cd5c62ace5ad9c074bc
ariaameri <ariaameri@live.com>
2020-08-01 22:15:13 -0400

    Fixes some small stuff in Trainer class.
    In __init__, sets epochs from self.config instead of config.
    Makes dummy dictionaries in train method.

--- nexdeepml/trainer/trainer.py
+++ nexdeepml/trainer/trainer.py
@@ -16,7 +16,7 @@ class Trainer(base.BaseEventManager, ABC):
         super().__init__(config)
 
         # Total number of epochs, total batch count, batch size, current epoch, and current batch number
-        self.epochs: int = config.epochs
+        self.epochs: int = self._config.epochs
         self.number_of_iterations: int = -1  # Has to be set by the data loader
         self.batch_size: int = -1  # Has to be set by the data loader
         self.epoch: int = 0
@@ -88,6 +88,10 @@ class Trainer(base.BaseEventManager, ABC):
         A dictionary containing the history of the process
 
         """
+
+        # Make dummy variables
+        train_dict = {}
+        val_dict = {}
 
         # Training
         if self.epoch == 0:
