b28008673f029dd42656853d5294a675a44b12f8
ariaameri <ariaameri@live.com>
2020-08-03 11:37:38 -0400

    Adds config to the initializer of the Model and ModelManager classes.

--- nexdeepml/model/model.py
+++ nexdeepml/model/model.py
@@ -6,19 +6,32 @@ from ..base import base
 class ModelManager(base.BaseEventManager, ABC):
     """Abstract class that manages neural network models."""
 
-    def __init__(self):
-        """Initializes this instance."""
+    def __init__(self, config: ConfigParser = None):
+        """Initialize the instance.
 
-        super().__init__()
+        Parameters
+        ----------
+        config : ConfigParser
+            The configuration needed for this instance
+
+        """
+
+        super().__init__(config)
 
 
 class Model(base.BaseWorker, ABC):
     """Abstract class that contains the neural network model."""
 
-    def __init__(self):
+    def __init__(self, config: ConfigParser = None):
         """Initializes the instance.
 
         Creates the model and takes care of initial tasks.
+
+        Parameters
+        ----------
+        config : ConfigParser
+            The configuration needed for this instance
+
         """
 
-        super().__init__()
+        super().__init__(config)
