3f0820377b9bb33e9232cbdd2ad588095122c5a9
ariaameri <ariaameri@live.com>
2020-08-04 19:02:58 -0400

    Adds logger.the_progress_bar.py. Initial commit for this file.

---
+++ nexdeepml/logger/the_progress_bar.py
@@ -0,0 +1,7 @@
+class TheProgressBar:
+    """Progress bar that takes care of additional prints to the screen while updating."""
+
+    def __init__(self):
+        """Initializes the instance."""
+
+        pass
