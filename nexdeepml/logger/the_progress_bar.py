e24231b83524a9e7b81ddf6242789662c74f40f6
ariaameri <ariaameri@live.com>
2020-08-04 19:11:40 -0400

    Adds fundamental methods to TheProgressBar class.

--- nexdeepml/logger/the_progress_bar.py
+++ nexdeepml/logger/the_progress_bar.py
@@ -1,7 +1,27 @@
 class TheProgressBar:
-    """Progress bar that takes care of additional prints to the screen while updating."""
+    """Progress bar that takes care of additional prints to the screen while updating.
+
+    This implementation is based on the alive_progress package.
+    """
 
     def __init__(self):
         """Initializes the instance."""
+
+        pass
+
+    def write(self, msg: str) -> None:
+        """Prints a message to the output.
+
+        Parameters
+        ----------
+        msg : str
+            The message to be written
+
+        """
+
+        pass
+
+    def flush(self):
+        """Flushes the screen."""
 
         pass
