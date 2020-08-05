9242b082d11c8b47393e49ba4ad6e6b4e55f5cf0
ariaameri <ariaameri@live.com>
2020-08-05 16:43:52 -0400

    Adds SamplePyTorchTrainer class.

--- nexdeepml/trainer/consumer.py
+++ nexdeepml/trainer/consumer.py
@@ -1,7 +1,9 @@
 from .trainer import Trainer
 from ..util.config import ConfigParser
 from ..callback.consumer import SampleCallbackManager
+from ..model.consumer import SamplePyTorchModelManager
 from typing import Dict
+import torch
 
 
 class SampleTrainer(Trainer):
@@ -25,3 +27,37 @@ class SampleTrainer(Trainer):
     def val_one_batch(self) -> Dict:
 
         return {}
+
+
+class SamplePyTorchTrainer(Trainer):
+    """Sample pyTorch Trainer that incorporates pyTorch neural model."""
+
+    def __init__(self, config: ConfigParser):
+
+        super().__init__(config)
+
+        self.create_workers()
+
+        self.model: SamplePyTorchModelManager
+        self.create_model()
+
+        self.optimizer = torch.optim.Adam(self.model.parameters())
+
+        self.criterion = torch.nn.MSELoss()
+
+    def create_workers(self):
+
+        self.workers['callback'] = SampleCallbackManager(self._config.callback, self)
+
+    def create_model(self):
+
+        self.model = SamplePyTorchModelManager(self._config.model)
+
+    def train_one_batch(self) -> Dict:
+
+        return {}
+
+    def val_one_batch(self) -> Dict:
+
+        return {}
+
