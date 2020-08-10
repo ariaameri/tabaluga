de548a24441d888b6a408f1d235c1d9dc98129c2
ariaameri <ariaameri@live.com>
2020-08-10 17:08:43 -0400

    DIRTY: Changes map method in DataHolder.

--- nexdeepml/util/data_holder.py
+++ nexdeepml/util/data_holder.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import List, Dict, Any
 from types import FunctionType
 from .config import ConfigParser
+import re
 
 
 class DataHolder(ConfigParser):
@@ -28,31 +29,36 @@ class DataHolder(ConfigParser):
 
         super().__init__(data_dict)
 
-    def map(self, function: FunctionType) -> DataHolder:
-        """Maps the function on the data and returns a new instance.
+    def map(self, function: FunctionType, filter_dict: Dict = {}, update_regex: str = ''):
 
-        Parameter
-        ---------
-        function : FunctionType
-            The function to be mapped on the data
+        return self.map_helper(function, filter_dict, update_regex)
 
-        Returns
-        -------
-        An instance of the class with new mapped data
+    def map_helper(self, function: FunctionType, filter_dict: Dict = {}, update_regex: str = '', bc: str = '', bc_meta: str = ''):
 
-        """
-
-        def map_helper(data):
+        def map_helper(data, key_name: str = ''):
 
             if type(data) is type(self):
-                return data.map(function)
+                out = data.map_helper(function, {}, update_regex, key_name, bc_meta)
             elif type(data) == list:
-                out = [map_helper(item) for item in data]
+                out = [map_helper(item, key_name) for item in data]
             else:
-                out = function(data)
+                out = function(data) if re.search(update_regex, key_name) else data
 
             return out
 
-        out = self.__class__({key: map_helper(value) for key, value in self.__dict__.items()})
+        bc_meta = bc_meta + self.__dict__.get('_meta', '')
 
-        return out
+        if self._filter_checker(filter_dict, bc, bc_meta) is True:
+            value = {key: map_helper(value, bc + f'.{key}') for key, value in self.__dict__.items()}
+            return self.__class__(value)
+
+        final_dict = {**self.__dict__,
+                      **{
+                          key: value.map_helper(function, filter_dict, update_regex, bc + f'.{key}', bc_meta)
+                          for key, value
+                          in self.__dict__.items()
+                          if type(value) is type(self)
+                        }
+                      }
+
+        return self.__class__(final_dict)
