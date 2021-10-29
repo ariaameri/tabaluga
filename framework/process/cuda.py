fc7ea75ac40b14ef68799381f397862f3148597d
aria <ariaameri@live.com>
2021-10-29 15:20:29 -0400

    Adds CUDAInformation class to process.

---
+++ framework/process/cuda.py
@@ -0,0 +1,66 @@
+from .process import Process
+from framework.util.config import ConfigParser
+import pynvml
+
+
+class CUDAInformation(Process):
+    """Class to get and print cuda information."""
+
+    def __init__(self, config: ConfigParser):
+
+        super().__init__(config)
+
+        # initialize the cuda nvml library
+        pynvml.nvmlInit()
+
+    def process(self, data=None):
+        """Retrieves information and prints them.
+
+        Parameters
+        ----------
+        data
+            Should be left empty
+
+        Returns
+        -------
+        * Nothing *
+
+        """
+
+        import re
+
+        device_info: str = ""
+
+        for i in range(pynvml.nvmlDeviceGetCount()):
+            device_info += f"* device {i}:\n"
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+
+            # get memory info
+            try:
+                info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+                device_info += f"\t - memory | " \
+                               f"used: {info.used/(1024**2)}MB, " \
+                               f"free: {info.free/(1024**2)}MB, " \
+                               f"total: {info.total/(1024**2)}MB\n"
+            except:
+                pass
+
+            # get temperature info
+            try:
+                info = pynvml.nvmlDeviceGetTemperature(handle, 0)
+                device_info += f"\t - temperature: {info}\u00B0C\n"
+            except:
+                pass
+
+        # indent the device info
+        device_info = re.sub(r'(^|\n)', '\n\t\t\t', device_info)
+
+        msg = f"\n\n" \
+              f"\t\u25A0 CUDA information is as follows:\n" \
+              f"\t\t \u00B7 driver version: {pynvml.nvmlSystemGetDriverVersion().decode('utf-8')}\n" \
+              f"\t\t \u00B7 cuda version: {pynvml.nvmlSystemGetCudaDriverVersion()}\n" \
+              f"\t\t \u00B7 device count: {pynvml.nvmlDeviceGetCount()}" \
+              f"\t\t {device_info}\n"
+
+        self._universal_log(msg, level='info')
+
