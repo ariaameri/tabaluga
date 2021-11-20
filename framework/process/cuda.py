01826aae5fa63c3cb3f8967c42ba1d6d3f419686
ariaameri <ariaameri@live.com>
2021-11-20 12:46:27 -0500

    Makes CUDAInformation process output colorful.

--- framework/process/cuda.py
+++ framework/process/cuda.py
@@ -1,3 +1,4 @@
+import colored
 from .process import Process
 from ..util.config import ConfigParser
 import pynvml
@@ -40,29 +41,30 @@ class CUDAInformation(Process):
             # get memory info
             try:
                 info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-                device_info += f"\t - memory | " \
-                               f"used: {info.used/(1024**2)}MB, " \
-                               f"free: {info.free/(1024**2)}MB, " \
-                               f"total: {info.total/(1024**2)}MB\n"
+                device_info += \
+                    f'\t \u2577 {colored.fg("cornsilk_1")}memory{colored.attr("reset")}: \n' \
+                    f'\t \u251C\u2500\u2500\u2500 {colored.fg("dark_orange_3a")}used: {info.used/(1024**2)}MB{colored.attr("reset")} \n' \
+                    f'\t \u251C\u2500\u2500\u2500 {colored.fg("green_3a")}free: {info.free/(1024**2)}MB{colored.attr("reset")} \n' \
+                    f'\t \u2514\u2500\u2500\u2500 {colored.fg("deep_sky_blue_4c")}total: {info.total/(1024**2)}MB{colored.attr("reset")}\n\n'
             except:
                 pass
 
             # get temperature info
             try:
                 info = pynvml.nvmlDeviceGetTemperature(handle, 0)
-                device_info += f"\t - temperature: {info}\u00B0C\n"
+                device_info += f'\t - {colored.fg("cornsilk_1")}temperature{colored.attr("reset")}: {info}\u00B0C\n'
             except:
                 pass
 
         # indent the device info
         device_info = re.sub(r'(^|\n)', '\n\t\t\t', device_info)
 
-        msg = f"\n\n" \
-              f"\t\u25A0 CUDA information is as follows:\n" \
-              f"\t\t \u00B7 driver version: {pynvml.nvmlSystemGetDriverVersion().decode('utf-8')}\n" \
-              f"\t\t \u00B7 cuda version: {pynvml.nvmlSystemGetCudaDriverVersion()}\n" \
-              f"\t\t \u00B7 device count: {pynvml.nvmlDeviceGetCount()}" \
-              f"\t\t {device_info}\n"
+        msg = f'\n\n'\
+              f'\t{colored.fg("turquoise_2")}\u25A0 {colored.fg("blue")}CUDA information{colored.attr("reset")} is as follows:\n'\
+              f'\t\t \u00B7 {colored.fg("cornflower_blue")}driver version{colored.attr("reset")}: {pynvml.nvmlSystemGetDriverVersion().decode("utf-8")}\n' \
+              f'\t\t \u00B7 {colored.fg("cornflower_blue")}cuda version{colored.attr("reset")}: {pynvml.nvmlSystemGetCudaDriverVersion()}\n' \
+              f'\t\t \u00B7 {colored.fg("cornflower_blue")}device count{colored.attr("reset")}: {pynvml.nvmlDeviceGetCount()}'\
+              f'\t\t {device_info}\n'
 
         self._log.info(msg)
 
