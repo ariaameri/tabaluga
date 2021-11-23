ca807987d965b6d456046dcd8fb05e959fd6f2a1
ariaameri <ariaameri@live.com>
2021-11-23 18:15:44 -0500

    Conforms TheProgressBarLogger with the new TPB changes and makes it ready for distributed mode.

--- framework/logger/logger.py
+++ framework/logger/logger.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
+import time
 from ..util.config import ConfigParser
-from .log_hug import LogHug
-from ..util.symbols_unicode import SYMBOL_UNICODE_CONFIG as SUC
+from ..util.panacea import Panacea
 from ..base.base import BaseEventManager
-from .the_progress_bar import TheProgressBarColored
-from .logger_sole import LoggerConsoleFile, Logger
+from .the_progress_bar import TheProgressBar, TheProgressBarParallelManager
+from ..communicator import mpi
+from .logger_sole import Logger
 from abc import ABC
-from typing import Dict, List, Union, Any
+from typing import Dict, Callable
 import numpy as np
-import re
-import colored
 
 
 class LoggerManager(BaseEventManager, ABC):
@@ -34,7 +33,6 @@ class TheProgressBarLogger(Logger):
     WARNING: It does not exactly behave the same way as the Logger class. It is just a wrapper/manager
                 class for the TheProgressBarLogger class.
     """
-    # TODO: Conform the TheProgressBarLogger or a new class with Logger class
 
     def __init__(self, config: ConfigParser = None):
         """Initialize the logger and the TheProgressBar instance.
@@ -53,7 +51,9 @@ class TheProgressBarLogger(Logger):
         super().__init__(config)
 
         # Create the instance of the TheProgressBar
-        self._the_progress_bar = TheProgressBarColored(self.console_file)
+        self._the_progress_bar = TheProgressBar(self.console_file)
+        self._the_progress_bar_manager = \
+            TheProgressBarParallelManager(self.console_file) if mpi.mpi_communicator.is_distributed() else None
 
         # The number of total items and epochs
         self._total: int = -1
@@ -77,6 +77,8 @@ class TheProgressBarLogger(Logger):
         """
 
         self._the_progress_bar.activate()
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.activate()
 
         return self
 
@@ -106,13 +108,13 @@ class TheProgressBarLogger(Logger):
         # set it!
         self._the_progress_bar.set_number_items(items)
 
-    def _custom_reset(self, total: int, return_to_line_number: int = 0) -> TheProgressBarLogger:
+    def _custom_reset(self, total: int = -np.inf, return_to_line_number: int = 0) -> TheProgressBarLogger:
         """Set the total number of iterations and resets the the_progress_bar.
 
         Parameters
         ----------
-        total : int
-            the total number of items expected.
+        total : int, optional
+            the total number of items expected. if not set, will be set to the default value.
         return_to_line_number: int, optional
             The number of the line to return to the beginning of, after the progress bar
 
@@ -120,41 +122,50 @@ class TheProgressBarLogger(Logger):
 
         self._the_progress_bar.reset(return_to_line_number=return_to_line_number)
         self._the_progress_bar.set_number_items(total)
+
+        # wait for all workers to be reset
+        if mpi.mpi_communicator.is_distributed():
+            mpi.mpi_communicator.barrier()
+            # wait a random time, just to be sure :D
+            time.sleep(1)
+            # now, reset the manager
+            self._the_progress_bar_manager.reset(return_to_line_number=return_to_line_number)
+
         self._total = total
 
         return self
 
-    def reset(self, total: int) -> TheProgressBarLogger:
+    def reset(self, total: int = -np.inf) -> TheProgressBarLogger:
         """Set the total number of iterations and prints and resets the the_progress_bar.
 
         Parameters
         ----------
-        total : int
-            the total number of items expected.
+        total : int, optional
+            the total number of items expected. if not set, will be set to the default value.
 
         """
 
         return self._custom_reset(total=total, return_to_line_number=-1)
 
-    def reset_bar_only(self, total: int) -> TheProgressBarLogger:
+    def reset_bar_only(self, total: int = -np.inf) -> TheProgressBarLogger:
         """Set the total number of iterations and resets only the bar of the the_progress_bar.
 
         Parameters
         ----------
-        total : int
-            the total number of items expected.
+        total : int, optional
+            the total number of items expected. if not set, will be set to the default value.
 
         """
 
         return self._custom_reset(total=total, return_to_line_number=0)
 
-    def reset_to_next_line(self, total: int) -> TheProgressBarLogger:
+    def reset_to_next_line(self, total: int = -np.inf) -> TheProgressBarLogger:
         """Set the total number of iterations and resets only the bar of the the_progress_bar.
 
         Parameters
         ----------
-        total : int
-            the total number of items expected.
+        total : int, optional
+            the total number of items expected. if not set, will be set to the default value.
 
         """
 
@@ -164,16 +175,22 @@ class TheProgressBarLogger(Logger):
         """Finishes and closes the TheProgressBar instance."""
 
         self._the_progress_bar.deactivate()
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.deactivate()
 
     def pause(self) -> None:
         """Pauses the TheProgressBar instance."""
 
         self._the_progress_bar.pause()
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.pause()
 
     def resume(self) -> None:
         """Resumes the TheProgressBar instance."""
 
         self._the_progress_bar.resume()
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.resume()
 
     def update(self, update_count: int, msg_dict: Dict = None) -> None:
         """Update the TheProgressBar progress bar with description set to message.
@@ -184,112 +201,61 @@ class TheProgressBarLogger(Logger):
             The amount that should be added to the TheProgressBar instance.
         msg_dict : Dict, optional
             Contains the dictionary message to be set as the progress bar description.
-            Will be passed to the _generate_message method, read there for more info.
         """
 
+        # first, update the bar
         self._the_progress_bar.update(update_count)
 
-        # message = self._generate_message(msg_dict)
-        message = self._generate_message_from_loghug(msg_dict)
-        self._the_progress_bar.set_description_after(message)
+        # # generate the message and aggregation data
+        # message, data = self._generate_message_from_loghug(msg_dict)
+        #
+        # # set all
+        # self._the_progress_bar.set_aggregation_data(data)
+        #
+        # self._the_progress_bar.set_description_after(message)
+        #
+        # self._the_progress_bar.set_description_short_after("yellow")
+        # self._the_progress_bar.set_description_short_before(f'At epoch {msg_dict["epoch"]}/{self._n_epochs} of {msg_dict["mode"]}:')
 
-    def _generate_message(self, msg_dict: Dict) -> str:
-        """Generates a string based on the input to be used as tqdm bar description.
-
-        If msg is None, empty string will be returned.
+    def set_aggregator_function_data(self, data: Panacea):
+        """
+        Sets the aggregator data that is used to process the aggregated string.
 
         Parameters
-        ---------
-        msg_dict : Dict
-            Dictionary containing the information to be used. Contains:
-                epoch: int
-                loss: float
-                val_loss: float, optional
+        ----------
+        data : Panacea
+            Aggregation data, here we require the data to be a Panacea subclass
+
         """
 
-        # Make a copy of the dictionary to modify it
-        msg_dict_copy = {**msg_dict}
+        self._the_progress_bar.set_aggregation_data(data)
 
-        title = ''
-
-        if msg_dict is None:
-            return title
-
-        # Find the length of the total epochs
-        # get and remove the 'epoch' item from the dictionary
-        # and reformat the string accordingly
-        ep_len = int(np.ceil(np.log10(self._n_epochs)))
-        epoch = msg_dict_copy.pop('epoch')
-        title = f'{colored.fg("green_4")}{SUC.heavy_teardrop_spoked_asterisk} '
-        title += f'{colored.fg("chartreuse_3a")}Epoch ' \
-                 f'{colored.fg("green_3a")}{epoch:{ep_len}d}' \
-                 f'{colored.fg("grey_27")}/' \
-                 f'{colored.fg("dark_green")}{self._n_epochs}' \
-                 f'{colored.attr("reset")}'
-
-        message_dict = {}
-
-        # Check if we have training values for logging, starting with 'train_'
-        # get the item from the dictionary and delete it
-        # Generate the message set and add it to the total message
-        train_items = {key[len('train_'):]: msg_dict_copy.pop(key) for key, value in msg_dict.items()
-                       if key.startswith('train_')}
-        if train_items:
-            message_dict = {**message_dict, 'Train': train_items}
-
-        # Check if we have validation values for logging, starting with 'train_'
-        # get the item from the dictionary and delete it
-        # Generate the message set and add it to the total message
-        val_items = {key[len('val_'):]: msg_dict_copy.pop(key) for key, value in msg_dict.items()
-                     if key.startswith('val_')}
-        if val_items:
-            message_dict = {**message_dict, 'Validation': val_items}
-
-        if msg_dict_copy:
-            message_dict = {**message_dict, 'Others': msg_dict_copy}
-
-        # Generate the message
-        message = LogHug(message_dict).str_representation_with_title(title=title)
-
-        # Indent the message
-        message = '\n\t' + '\n\t'.join(message.split(f'\n'))
-
-        return message
-
-    def _generate_message_from_loghug(self, msg_dict: Dict) -> str:
-        """Generates a string based on the input to be used as tqdm bar description.
-
-        If msg is None, empty string will be returned.
+    def set_aggregator_function_full(self, func: Callable[[list[Panacea]], str]):
+        """
+        Sets the aggregator function that is used to process the aggregated data and give full output.
 
         Parameters
-        ---------
-        msg_dict : Dict
-            Dictionary containing the information to be used. Contains:
-                epoch: int
-                loss: float
-                val_loss: float, optional
+        ----------
+        func : Callable[[list[Panacea, str]]]
+            function that is given a list of to-be-aggregated data and should return the full aggregation.
+
         """
 
-        # Find the length of the total epochs
-        # get and remove the 'epoch' item from the dictionary
-        # and reformat the string accordingly
-        ep_len = int(np.ceil(np.log10(self._n_epochs)))
-        epoch = msg_dict.get('epoch')
-        title = f'\n' \
-                f'{colored.fg("green_4")}{SUC.heavy_teardrop_spoked_asterisk} '
-        title += f'{colored.fg("chartreuse_3a")}Epoch ' \
-                 f'{colored.fg("green_3a")}{epoch:{ep_len}d}' \
-                 f'{colored.fg("grey_27")}/' \
-                 f'{colored.fg("dark_green")}{self._n_epochs}' \
-                 f'{colored.attr("reset")}'
+        self._the_progress_bar.set_aggregator_function_full(func)
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.set_aggregator_function_full(func)
 
-        # If there is a stat entry, use it
-        if msg_dict.get('stat') is not None:
-            message = msg_dict.get('stat').str_representation_with_title(title=title)
-        else:
-            message = title
+    def set_aggregator_function_short(self, func: Callable[[list[Panacea]], str]):
+        """
+        Sets the aggregator function that is used to process the aggregated data and give short output.
 
-        # Indent the messages once
-        message = re.sub(r'(^|\n)', r'\1\t', message)
+        Parameters
+        ----------
+        func : Callable[[list[Panacea, str]]]
+            function that is given a list of to-be-aggregated data and should return the short aggregation.
 
-        return message
+        """
+
+        self._the_progress_bar.set_aggregator_function_short(func)
+        if mpi.mpi_communicator.is_distributed():
+            self._the_progress_bar_manager.set_aggregator_function_short(func)
