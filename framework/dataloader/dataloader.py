c74a0694a40099cb70b05335e5eaaa9700dfd674
ariaameri <ariaameri@live.com>
2021-11-29 12:48:20 -0500

    Adds check_terminal_focused in util.ut

--- framework/dataloader/dataloader.py
+++ framework/dataloader/dataloader.py
@@ -8,6 +8,16 @@ import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 
 
+# a mapping between the column concepts and their names
+metadata_columns = {
+    'folder_path': 'folder_path',
+    'folder_name': 'folder_name',
+    'file_name': 'file_name',
+    'file_extension': 'file_extension',
+    'path': 'path',
+}
+
+
 class DataManager(base.BaseEventManager, ABC):
     """This abstract class manages the DataLoader or DataLoader Managers.
 
@@ -24,13 +34,10 @@ class DataManager(base.BaseEventManager, ABC):
 
         super().__init__(config)
 
-        self._config = self._re_config(self._config)
-
-        # Folders containing the data
-        self._folders: List[str] = []
+        self._check_process_config()
 
         # Get the input type
-        self._input_type: str = self._config.get('input_type')
+        self._input_type: str = self._config.get_or_else('input_type', 'folder_path')
 
         # Get random seed and shuffle boolean
         self._seed = self._config.get_or_else('seed', None)
@@ -54,149 +61,48 @@ class DataManager(base.BaseEventManager, ABC):
         self.workers['validation']: DataLoaderManager
         self.workers['test']: DataLoaderManager
 
-        # Create general and train, val, test metadata
+        # Create and modify general and train, val, test metadata
         self.create_metadata()
+        self.modify_metadata()
+        self._generate_train_val_test_metadata()
 
         # Create workers
         self.create_workers()
 
-    def _re_config(self, config: ConfigParser) -> ConfigParser:
-        """Reconfigures the config file for this class.
-
-        Returns
-        -------
-        Reconfigured config file as a ConfigParser instance
-
-        """
+    def _check_process_config(self) -> None:
+        """Reconfigures the config file for this class."""
 
         # Spread multithreading one level down
         # Didn't choose only one level down because 'train' and 'val' entries might not exist
-        if config.get_option('multithreading').is_defined():
-            config = config.update(
+        if self._config.get_option('multithreading').is_defined():
+            self._config = self._config.update(
                 {},
                 {'$set_on_insert': {
-                    'train.multithreading': config.get('multithreading'),
-                    'val.multithreading': config.get('multithreading'),
+                    'train.multithreading': self._config.get('multithreading'),
+                    'val.multithreading': self._config.get('multithreading'),
                 }})
-
-        return config
 
     def create_metadata(self) -> None:
         """Checks how to create metadata from input source and create train, validation, and test metadata."""
 
         # Read and create the data metadata
         if self._input_type == 'folder_path':
-            self._build_metadata_from_folder()
+            metadata_generator = FolderReader(self._config)
         elif self._input_type == 'mongo':
-            self._build_metadata_from_mongo()
+            raise NotImplementedError
+        else:
+            raise ValueError(f"input type of {self._input_type} not recognized")
 
-        # Regroup the metadata based on the criteria of file name
-        self._regroup_metadata('file_name')
+        # get the metadata
+        self.metadata = metadata_generator.build_and_get_metadata()
 
-        # Generate the train, validation, and test metadata
-        self._generate_train_val_test_metadata()
+    def modify_metadata(self) -> None:
+        """Modifies the metadata that this instance holds."""
 
-    def _check_file(self, file_path: Path) -> bool:
-        """"Helper function to check a single file.
+        metadata_manipulator = MetadataManipulator(metadata=self.metadata)
 
-        Parameters
-        ----------
-        file_path : Path
-            The path of the file
-        """
-
-        # Check that the file is not a folder
-        check = file_path.is_file()
-
-        # Check criteria for the file name
-        check &= self._filter_file_name(file_path.name)
-
-        return check
-
-    def _filter_file_name(self, file_name: str) -> bool:
-        """"Helper function to filter a single file based on its name and criteria.
-
-        Parameters
-        ----------
-        file_name : str
-            The path of the file
-        """
-
-        check = False if file_name.startswith('.') else True
-        # Take care of MacOS special files
-        check &= file_name not in ['Icon\r']
-
-        return check
-
-    def _build_metadata_from_folder(self) -> None:
-        """This method goes over the specified folders, read files and creates a pandas data frame from them."""
-
-        # Folders containing the data
-        self._folders: List[str] = self._config.get('folders')
-
-        # Check if folder list is given
-        if self._folders is None:
-            raise Exception("No folders given to read files from!")
-        for folder in self._folders:
-            if Path(folder).exists() is False:
-                raise Exception(f'The folder {folder} does not exist!')
-
-        file_nested_paths = [list(Path(folder).iterdir()) for folder in self._folders]  # File names in nested lists
-
-        # Flatten the file names and make absolute paths
-        file_paths = [file_path
-                      for file_paths in file_nested_paths
-                      for file_path in file_paths]
-        # Check each file based on the criteria
-        file_paths = [file_path for file_path in file_paths if self._check_file(file_path)]
-        # Retrieve the folder path and file names
-        folder_paths = [file_name.parent for file_name in file_paths]
-        folder_names = [folder_path.name for folder_path in folder_paths]
-        file_names = [file_path.stem for file_path in file_paths]
-        file_extensions = [file_path.suffix.lower() for file_path in file_paths]
-
-        # Create data frame of all the files in the folder
-        self.metadata = pd.DataFrame({
-            'folder_path': [str(item) for item in folder_paths],
-            'folder_name': folder_names,
-            'file_name': file_names,
-            'file_extension': file_extensions,
-            'path': [str(item) for item in file_paths]
-        })
-
-    def _build_metadata_from_mongo(self) -> None:
-        """Creates metadata based on the information retrived from mongoDB."""
-
-        pass
-
-    def _regroup_metadata(self, criterion=None) -> None:
-        """Groups the metadata.
-
-        Each group of data (e.g. containing data and label) should have.
-        Each group must have its own unique index, where indices are range.
-        Each group will be recovered by metadata.loc[index]
-
-        Parameters
-        ----------
-        criterion : str or List[str]
-            The name of the columns based on which the metadata should be categorized
-
-        """
-
-        if criterion is None:
-            return
-
-        # Group based on the criterion
-        metadata = self.metadata.groupby(criterion).apply(lambda x: x.reset_index(drop=True))
-
-        # Rename the indices to be range
-        # Also rename the index level 0 name to be 'index' (instead of `criterion`)
-        metadata = metadata.rename(
-            index={key: value for value, key in enumerate(metadata.index.get_level_values(0).unique(), start=0)}
-        )
-        metadata.index.names = [None, *metadata.index.names[1:]]
-
-        self.metadata = metadata
+        # regroup the metadata
+        self.metadata = metadata_manipulator.modify()
 
     def _generate_train_val_test_metadata(self) -> None:
         """The method creates train, validation, and test metadata."""
@@ -254,6 +160,164 @@ class DataManager(base.BaseEventManager, ABC):
             worker.set_batch_size(batch_size)
 
 
+class MetadataManipulator(base.BaseWorker):
+    """Class to modify and manipulate metadata."""
+
+    def __init__(self, metadata: pd.DataFrame, config: ConfigParser = None):
+
+        super().__init__(config)
+
+        # metadata storage
+        self.metadata = metadata
+
+    def modify(self) -> pd.DataFrame:
+        """Runs the whole modification pipeline and returns the result."""
+
+        # regroup based on the file name
+        metadata = self.regroup_metadata(criterion=metadata_columns['file_name'])
+
+        return metadata
+
+    def regroup_metadata(self, criterion=None) -> pd.DataFrame:
+        """Groups the metadata.
+
+        Each group of data (e.g. containing data and label) should have.
+        Each group must have its own unique index, where indices are range.
+        Each group will be recovered by metadata.loc[index]
+
+        Parameters
+        ----------
+        criterion : str or List[str]
+            The name of the columns based on which the metadata should be categorized
+
+        Returns
+        -------
+        pd.DataFrame
+            the result
+
+        """
+
+        if criterion is None:
+            return self.metadata
+
+        # check if criterion is a column concept
+        if isinstance(criterion, str) and criterion not in self.metadata.columns:
+            raise ValueError(f"criterion '{criterion}' does not exists as a metadata column.")
+        elif isinstance(criterion, list) and any([(c not in self.metadata.columns) for c in criterion]):
+            raise ValueError(f"part of criterion '{criterion}' does not exists as a metadata column.")
+
+        # Group based on the criterion
+        metadata = self.metadata.groupby(criterion).apply(lambda x: x.reset_index(drop=True))
+
+        # Rename the indices to be range
+        # Also rename the index level 0 name to be 'index' (instead of `criterion`)
+        metadata = metadata.rename(
+            index={key: value for value, key in enumerate(metadata.index.get_level_values(0).unique(), start=0)}
+        )
+        metadata.index.names = [None, *metadata.index.names[1:]]
+
+        self.metadata = metadata
+
+        return metadata
+
+
+class FolderReader(base.BaseWorker):
+    """This class is a helper class that reads metadata from a path (folder)."""
+
+    def __init__(self, config: ConfigParser = None):
+
+        super().__init__(config)
+
+        # Folders containing the data
+        self._folders: List[str] = []
+
+        # check and populate the folder metadata
+        self._check_populate_folder_metadata()
+
+    def _check_populate_folder_metadata(self):
+        """Checks given folders for sanity and existence."""
+
+        # Folders containing the data
+        self._folders: List[str] = self._config.get_or_else('folders', None)
+
+        # Check if folder list is given
+        if self._folders is None:
+            raise ValueError("No folders given to read files from!")
+        for folder in self._folders:
+            if Path(folder).exists() is False:
+                raise FileNotFoundError(f'The folder {folder} does not exist!')
+
+    def build_and_get_metadata(self) -> pd.DataFrame:
+        """
+        This method goes over the specified folders, read files and creates and returns a pandas data frame from them.
+
+        Returns
+        -------
+        pd.DataFrame
+            pandas dataframe of the information
+
+        """
+
+        # File names in nested lists
+        # only one level deep
+        file_nested_paths = [list(Path(folder).iterdir()) for folder in self._folders]
+
+        # Flatten the file names and make absolute paths
+        file_paths = [file_path
+                      for file_paths in file_nested_paths
+                      for file_path in file_paths]
+        # Check each file based on the criteria
+        file_paths = [file_path for file_path in file_paths if self._check_file(file_path)]
+        # Retrieve the folder path and file names
+        folder_paths = [file_name.parent for file_name in file_paths]
+        folder_names = [folder_path.name for folder_path in folder_paths]
+        file_names = [file_path.stem for file_path in file_paths]
+        file_extensions = [file_path.suffix.lower() for file_path in file_paths]
+
+        # Create data frame of all the files in the folder
+        metadata = pd.DataFrame({
+            metadata_columns['folder_path']: [str(item) for item in folder_paths],
+            metadata_columns['folder_name']: folder_names,
+            metadata_columns['file_name']: file_names,
+            metadata_columns['file_extension']: file_extensions,
+            metadata_columns['path']: [str(item) for item in file_paths]
+        })
+
+        return metadata
+
+    def _check_file(self, file_path: Path) -> bool:
+        """"Helper function to check a single file.
+
+        Parameters
+        ----------
+        file_path : Path
+            The path of the file
+        """
+
+        # Check that the file is not a folder
+        check = file_path.is_file()
+
+        # Check criteria for the file name
+        check &= self._filter_file_name(file_path.name)
+
+        return check
+
+    def _filter_file_name(self, file_name: str) -> bool:
+        """"Helper function to filter a single file based on its name and criteria.
+
+        Parameters
+        ----------
+        file_name : str
+            The path of the file
+        """
+
+        check = False if file_name.startswith('.') else True
+        # Take care of MacOS special files
+        check &= file_name not in ['Icon\r']
+
+        return check
+
+
 class DataLoaderManager(base.BaseEventManager, ABC):
     """This abstract class manages the data loaders and gets input from DataManager."""
 
@@ -288,7 +352,7 @@ class DataLoaderManager(base.BaseEventManager, ABC):
         """Checks how to create metadata from input source and create train, validation, and test metadata."""
 
         # Make a selection of the metadata
-        selection = [self._check_file(file_path) for file_path in self.metadata['path']]
+        selection = [self._check_file(file_path) for file_path in self.metadata[metadata_columns['path']]]
 
         # Update the metadata
         self.metadata = self.metadata.iloc[selection]
@@ -461,7 +525,7 @@ class DataLoader(base.BaseEventWorker, ABC):
         """Checks how to create metadata from input source and create train, validation, and test metadata."""
 
         # Make a selection of the metadata
-        selection = [self._check_file(file_path) for file_path in self.metadata['path']]
+        selection = [self._check_file(file_path) for file_path in self.metadata[metadata_columns['path']]]
 
         # Update the metadata
         self.metadata = self.metadata.iloc[selection]
@@ -632,3 +696,39 @@ class DataLoader(base.BaseEventWorker, ABC):
         data = self.load_data(metadata)
 
         return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
