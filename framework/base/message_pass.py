a42960478c98baee4a9d8730b16a8e219a61c69a
ariaameri <ariaameri@live.com>
2022-01-22 09:58:47 -0500

    Adds MessagePasserBase and modifies ask pattern in message_pass.py.

--- framework/base/message_pass.py
+++ framework/base/message_pass.py
@@ -1,13 +1,12 @@
-import multiprocessing.connection
 import queue
 import random
 import threading
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import TypeVar, Generic, Optional
+from typing import TypeVar, Generic, Optional, Callable, Any
 
 # type of message we accept
 MessageType = TypeVar("MessageType")
+MessagePasserBaseSubclass = TypeVar("MessagePasserBaseSubclass", bound="MessagePasserBase")
 
 
 class ReceiveResults:
@@ -20,23 +19,40 @@ class ReceiveResults:
         """Indicating the same behavior for receiving"""
 
         pass
-    sameReceive = SameReceive()
+    same_receive = SameReceive()
 
     class EndReceive(BaseReceiveResult):
         """Indicating end of receiving"""
 
         pass
-    endReceive = EndReceive()
+    end_receive = EndReceive()
 
 
-class MessagePasser(Generic[MessageType], ABC):
+class MessagePasserBase(Generic[MessageType], ABC):
+    """Abstract parent trait implementing message internal passing interface."""
+
+    def __init__(self):
+        """Initializer."""
+
+        # make a queue for message passing
+        self._message_pass_queue: queue.Queue[MessageType] = queue.Queue()
+
+    @abstractmethod
+    def _receive(self) -> None:
+        """Main method. Receives the messages sent and acts upon them."""
+
+        raise NotImplementedError
+
+
+class MessagePasser(MessagePasserBase[MessageType], ABC):
     """Trait implementing message internal passing interface."""
 
     def __init__(self):
         """Initializer."""
 
-        # make a queue for the internal messages and a thread to run it
-        self._message_pass_queue: queue.Queue = queue.Queue()
+        super().__init__()
+
+        # make a thread to run it
         self._message_pass_thread: Optional[threading.Thread] = None
         self._message_pass_thread_ident: Optional[int] = None
 
@@ -116,7 +132,7 @@ class MessagePasser(Generic[MessageType], ABC):
 
         self._message_pass_queue.put(message)
 
-    def ask(self, message: MessageType) -> MessageType:
+    def ask(self, message_factory: Callable[[MessagePasserBaseSubclass], MessageType]) -> Any:
         """
         Sends the message to self and wait for its reply.
 
@@ -124,12 +140,12 @@ class MessagePasser(Generic[MessageType], ABC):
 
         Parameters
         ----------
-        message : MessageType
-            the message to be sent for later processing
+        message_factory : Callable[['MessagePasserBase'], MessageType]
+            the message factory. it should be a function that takes a reference to a MessagePasser and returns the message
 
         Returns
         -------
-        MessageType
+        Any
             the result
 
         """
@@ -138,40 +154,82 @@ class MessagePasser(Generic[MessageType], ABC):
         if self._message_pass_thread_ident is not None and threading.get_ident() == self._message_pass_thread_ident:
             raise RuntimeError("ask is called from the same thread as listener.")
 
-        return _Asker(self).ask(message)
+        return _Asker(self).ask(message_factory)
 
 
-class _Asker(Generic[MessageType]):
+def ask(
+        message_passer: MessagePasser[MessageType],
+        message_factory: Callable[[MessagePasserBaseSubclass], MessageType]
+) -> Any:
+    """
+    Asks the message passer a message and blocks until gets back the result.
 
-    def __init__(self, reference: MessagePasser[MessageType]):
+    Parameters
+    ----------
+    message_passer : MessagePasser
+        the message passer to ask
+    message_factory :  Callable[['MessagePasserBase'], MessageType]
+        the message factory. it should be a function that takes a reference to a MessagePasser and returns the message
+
+    Returns
+    -------
+    Any
+        the result
+
+    """
+
+    return _Asker(message_passer).ask(message_factory)
+
+
+MessageTypeAsk = TypeVar("MessageTypeAsk")
+
+
+class _Asker(MessagePasserBase[Any]):
+
+    def __init__(self, reference: MessagePasser[MessageTypeAsk]):
         """
         Initializer.
 
         Parameters
         ----------
-        reference : MessagePasser
+        reference : MessagePasser[MessageTypeAsk]
             the message passer to ask from
         """
 
-        # make a queue for the internal messages and a thread to run it
-        self._message_pass_queue: queue.Queue = queue.Queue()
-        # keep the message passer for asking
-        self.ask_reference: MessagePasser[MessageType] = reference
+        super().__init__()
 
-    def _receive(self) -> MessageType:
+        # keep the message passer for asking
+        self.ask_reference: MessagePasser[MessageTypeAsk] = reference
+
+    def _receive(self) -> Any:
         """Main method. Receives the messages and returns it."""
 
         # get only a single message and return it
 
         # get the message
-        message: MessageType = self._message_pass_queue.get()
+        # notice that we get an Any message because we do not know the type of the sender's message
+        message: Any = self._message_pass_queue.get()
 
         return message
 
-    def ask(self, message: MessageType) -> MessageType:
+    def ask(self, message_factory: Callable[['_Asker'], MessageType]) -> Any:
+        """
+        Asks for a value, waits for it, and returns it.
 
-        # first tell the message
-        self.ask_reference.tell(message)
+        Parameters
+        ----------
+        message_factory : Callable[['MessagePasserBase'], MessageType]
+        the message factory. it should be a function that takes a reference to a MessagePasser and returns the message
+
+        Returns
+        -------
+        Any
+            the result
+
+        """
+
+        # first tell the message and make it send to self
+        self.ask_reference.tell(message_factory(self))
 
         # now, wait for the result
         result = self._receive()
