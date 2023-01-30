1aeee1ec5b330283ba94cd2ccbc9dc85b136d683
ariaameri <ariaameri@live.com>
2023-01-29 19:15:04 -0500

    Deletes _regroup_metadata of DataLoaderManager. Now all the functionality is in MetadataManipulator's.

--- framework/dataloader/dataloader.py
+++ framework/dataloader/dataloader.py
@@ -1932,41 +1932,6 @@ class DataLoaderManager(base.BaseEventManager, ABC):
 
         return metadata
 
-    def _regroup_metadata(self, metadata: pd.DataFrame, criterion=None, reset_index: bool = True) -> pd.DataFrame:
-        """Groups the metadata.
-
-        Each group of data (e.g. containing data and label) should have.
-        Each group must have its own unique index, where indices are range.
-        Each group will be recovered by metadata.loc[index]
-
-        Parameters
-        ----------
-        metadata : pd.DataFrame
-            metadata to use
-        criterion : str or List[str]
-            The name of the columns based on which the metadata should be categorized
-        reset_index : bool, optional
-            Whether to reset the level-0 indexing to range. If not given, will reset
-
-        """
-
-        if criterion is None:
-            return metadata
-
-        # Group based on the criterion
-        metadata = metadata.groupby(criterion).apply(lambda x: x.reset_index(drop=True))
-
-        # Rename the indices to be range
-        # Also rename the index level 0 name to be 'index' (instead of criterion)
-        if reset_index:
-            metadata = metadata.rename(
-                index={key: value for value, key in enumerate(metadata.index.get_level_values(0).unique(), start=0)},
-                level=0,
-            )
-            metadata.index.names = [None, *metadata.index.names[1:]]
-
-        return metadata
-
     def _check_file(self, file_path: str) -> bool:
         """"Helper function to check a single file.
 
